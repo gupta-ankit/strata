ac7281bc111acc29aa471669da4433db74a156b3
Ankit Gupta <ankit.gupta2801@gmail.com>
2022-11-13 09:14:34 +0530

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_navbar_vars
+
+  private
+  def set_navbar_vars
+    unless params[:strata_plan_id].nil? || current_user.nil?
+      @strata_plan = current_user.strata_plans.find(params[:strata_plan_id])
+      @chat_rooms = @strata_plan.chat_rooms
+    end
+  end
 end
--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,8 +1,7 @@
 class ChatRoomsController < ApplicationController
 
   def show
-    @chat_room = current_user.strata_plans.find(params[:strata_plan_id])
-                             .chat_rooms.find(params[:id])
+    @chat_room = @chat_rooms.find(params[:id])
     @new_message = ChatMessage.new(chat_room: @chat_room)
   end
 end
