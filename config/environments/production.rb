2de5c5308de28998c89d0f160b2dba0d14d2aa67
Ankit Gupta <ankit.gupta2801@gmail.com>
2022-11-20 10:58:53 +0530

    added mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,3 +93,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'strata.herokuapp.com' }
 end
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'strata.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
