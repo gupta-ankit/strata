27e5fd2b042582f0227e54013c7f93477a3b427f
Ankit Gupta <ankit.gupta2801@gmail.com>
2022-11-20 10:22:08 +0530

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user = User.where(email: "test@example.com").first_or_create! do |u|
-  u.id = SecureRandom.uuid
-  u.password = "password"
+users = []
+5.times do |i|
+  user = User.where(email: "test#{i}@example.com").first_or_create! do |u|
+    u.id = SecureRandom.uuid
+    u.password = "password"
+  end
+  users << user
 end
+user = users[0]
 
 strata_plan = StrataPlan.where(name: "StrataPlan123").first_or_create!
 residential_unit = ResidentialUnit.where(strata_plan: strata_plan, name: "101").first_or_create!
@@ -17,9 +22,10 @@ resident = Resident.where(user: user, residential_unit: residential_unit).first_
 admin = StrataAdmin.where(strata_plan: strata_plan, user: user, role: :caretaker).first_or_create!
 chat_room = ChatRoom.where(name: "Chat", strata_plan_id: strata_plan.id,
                            creator_id: user.id).first_or_create!
-ChatMessage.where(author: user, chat_room:
-  chat_room).first_or_create! do |m|
-  m.content = "Hello, World!"
+5.times do |i|
+  ChatMessage.where(author: users[i], chat_room: chat_room).first_or_create! do |m|
+    m.content = "Hello, World#{i}!"
+  end
 end
 
 Issue.where(reporter: user, strata_plan:
